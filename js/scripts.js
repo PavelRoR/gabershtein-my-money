c3863e43c89085d6d134dad4cf107dfcb405f3a9
PavelRoR <pavelvp2182@yandex.ru>
2022-04-09 14:30:48 +0500

    Course page is all over done

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,95 @@
 $(document).ready(function () {
-    // Работа форм
-    $(function () {
-        var check = $('.check', this),
-            email = $('.input-mail', this),
-            emCorr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,10}$/,
-            message = $('.alert-message', this),
-            button = $('.button-form', this);
-        email.keyup(function () {
-            if (emCorr.test($(this).val())) {
-                $(this).parent().parent('.form').children('.button').css('backgroundColor', '#48817B');
-            }
-            if (!$(this).val() || !emCorr.test($(this).val())) {
-                $(this).parent().parent('.form').children('.button').css('backgroundColor', '#C7C7C7')
-                // button.css('backgroundColor', '#C7C7C7')
-            }
-        })
-        $(".form").on("submit", function () {
-            var check = $('.check', this),
-                message = $('.alert-message', this),
-                reNone = /.+/,
-                email = $('.input-mail', this);
+        function cookClear(){
+            $('.button-cookies-no').on('click', function(){
+                localStorage.removeItem('cook','cookYes');
+                $.fancybox.close( $('.modal-cookies'));
+            });
+        }
+        function cookSet(){
+            $('.button-cookies-yes').on('click', function(){
+                localStorage.setItem('cook','cookYes');
+                $.fancybox.close( $('.modal-cookies'));
+            });
+        }
+        cookClear();
+        cookSet();
 
-            if (!email.val().match(reNone)) {
-                message.text('Введите email').slideDown(500);
-                return false;
-            }
-            if (!check.prop("checked")) {
-                check.next().css({
-                    'color': 'red',
-                    'transition': 'all .4s ease'
-                });
-                check.next().children().css({
-                    'color': 'red',
-                    'transition': 'all .4s ease'
-                });
-                message.text('Подтвердите соглашение').slideDown(500);
-                return false;
-            }
-            if (email.val() && check) {
-                return true
-            }
+        $('.button-cookies-yes').attr('disabled', !$('.check-cookies').prop('checked'));
+        $('.check-cookies').change(function(){
+            $('.button-cookies-yes').attr('disabled', !$('.check-cookies').prop('checked'));
         });
-        email.click(function () {
-            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
-            message.slideUp(500);
-        });
-        check.click(function () {
-            check.next().css({
-                "color": "#9C9C9C",
-                'transition': 'all .4s ease'
-            });
-            check.next().children().css({
-                "color": "#9C9C9C",
-                'transition': 'all .4s ease'
-            });
-            message.slideUp(500);
-        });
-    });
+        if (!localStorage.getItem('cook','cookYes')){
+            $.fancybox.open( $('.modal-cookies'));
+            cookSet();
+            cookClear();
+            
+        }
+        // else {
+        //     // !$.fancybox.open( $('.modal-cookies'));
+        //     // cookSet();
+        //     // cookClear();
+        //     alert('ji');
+        // }
+    // Работа форм
+    // $(function () {
+    //     var check = $('.check', this),
+    //         email = $('.input-mail', this),
+    //         emCorr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,10}$/,
+    //         message = $('.alert-message', this),
+    //         button = $('.button-form', this);
+    //     email.keyup(function () {
+    //         if (emCorr.test($(this).val())) {
+    //             $(this).parent().parent('.form').children('.button').css('backgroundColor', '#48817B');
+    //         }
+    //         if (!$(this).val() || !emCorr.test($(this).val())) {
+    //             $(this).parent().parent('.form').children('.button').css('backgroundColor', '#C7C7C7')
+    //             // button.css('backgroundColor', '#C7C7C7')
+    //         }
+    //     })
+    //     $(".form").on("submit", function () {
+    //         var check = $('.check', this),
+    //             message = $('.alert-message', this),
+    //             reNone = /.+/,
+    //             email = $('.input-mail', this);
+
+    //         if (!email.val().match(reNone)) {
+    //             message.text('Введите email').slideDown(500);
+    //             return false;
+    //         }
+    //         if (!check.prop("checked")) {
+    //             check.next().css({
+    //                 'color': 'red',
+    //                 'transition': 'all .4s ease'
+    //             });
+    //             check.next().children().css({
+    //                 'color': 'red',
+    //                 'transition': 'all .4s ease'
+    //             });
+    //             message.text('Подтвердите соглашение').slideDown(500);
+    //             return false;
+    //         }
+    //         if (email.val() && check) {
+    //             return true
+    //         }
+    //     });
+    //     email.click(function () {
+    //         // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+    //         message.slideUp(500);
+    //     });
+    //     check.click(function () {
+    //         check.next().css({
+    //             "color": "#9C9C9C",
+    //             'transition': 'all .4s ease'
+    //         });
+    //         check.next().children().css({
+    //             "color": "#9C9C9C",
+    //             'transition': 'all .4s ease'
+    //         });
+    //         message.slideUp(500);
+    //     });
+    // });
     // модалка
-    // $('.button-up').fancybox();
+    $('.button-up').fancybox();
     // слайдер с видео-отзывами
     $('.revs-slider').slick({
         autoplay: false,
